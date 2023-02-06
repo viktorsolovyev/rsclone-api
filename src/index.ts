28f0c484baa597dfd10b1ae6790796fdf4c4e280
Viktor Solovyev <barn-mih@rambler.ru>
2023-02-07 00:10:53 +0300

    feat: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = 5000;
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+const port = process.env.PORT;
+app.get("/", (request, response) => {
+  response.send("Hello world!");
 });
 app.listen(port, () => console.log(`Running on port ${port}`));
